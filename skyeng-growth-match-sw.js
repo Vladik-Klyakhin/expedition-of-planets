28ee0362f604428df5d0ed1a48c4819ff66eb3fc
Vladik-Klyakhin <129801444+Vladik-Klyakhin@users.noreply.github.com>
2026-08-11 21:25:28 +0300

    Обновляю PWA-кэш для автоматического получения новых версий

--- skyeng-growth-match-sw.js
+++ skyeng-growth-match-sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'skyeng-growth-match-v2';
+const CACHE_NAME = 'skyeng-growth-match-v3';
 const ASSETS = [
   './index.html',
   './skyeng-growth-match.webmanifest',
@@ -11,7 +11,11 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
